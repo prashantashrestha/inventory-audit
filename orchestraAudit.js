45dd5ce9eb557759fb9e9fa3c26ffea160a207d9
Prashanta Shrestha <prashanta.shrestha@Prasbrozs-Macbook-Pro.local>
2025-11-16 01:34:46 +0545

    fix: updated pools inside of xen host for better mapping

--- orchestraAudit.js
+++ orchestraAudit.js
@@ -8,9 +8,15 @@ const AUTH_TOKEN = '4b-DxYjypA3I6YeRm-l_C37c_Mrq9wicnl_gCowfcnM';
 let vmDetails = {};
 (async () => {
   try {
-    vmDetails = await fetchVms();
-
-    await fetchHostsAndResidentVms();
+    const poolName = await fetchPools();
+    const vmDetails = await fetchVms();
+    //Map pool name to vm details
+    const newVmDetails = Object.fromEntries(
+     Object.entries(vmDetails).map(([uuid,vmList]) => [
+      poolName[uuid],vmList
+     ])
+    );
+    fs.writeFileSync("xo_vms.json",JSON.stringify(newVmDetails, null, 2));
   } catch (err) {
     console.error('Error during initialization:', err);
   }
@@ -30,22 +36,17 @@ async function fetchWithAuth(url) {
   }
 }
 
-// Fetch all hosts and their resident VMs
-async function fetchHostsAndResidentVms() {
+// Fetch all Pool Names
+async function fetchPools() {
     try {
-        const url = `${XO_HOST}/rest/v0/hosts?fields=name_label`;
+        const url = `${XO_HOST}/rest/v0/pools?fields=name_label,uuid`;
         const hosts = await fetchWithAuth(url);
         const hostsInfo = {};
         for (const host of hosts) {
-            const residentVms = await fetchResidentVms(host.href);
-            hostsInfo[host.name_label] = residentVms.map(vm => ({
-                name_label: vm.name_label,
-                power_state: vm.power_state,
- //               mainIpAddress: vm.mainIpAddress
-            }));
+            hostsInfo[host.uuid] = host.name_label
         }
+	return hostsInfo;
 
-        fs.writeFileSync("xo_vms.json",JSON.stringify(hostsInfo, null, 2));
     } catch (err) {
         console.error('Error fetching Hosts and Resident VMs:', err);
     }
@@ -57,7 +58,6 @@ async function fetchResidentVms(hostUrl) {
     const url = `${XO_HOST}${hostUrl}`;
     const hostData = await fetchWithAuth(url);
     if (Array.isArray(hostData.residentVms)) {
-	console.log(hostData.residentVms.length)
       return hostData.residentVms
         .map(vmId => vmDetails[vmId])
         .filter(Boolean); // Filter out undefined VMs
@@ -73,17 +73,24 @@ async function fetchResidentVms(hostUrl) {
 // Fetch all VMs and transform into a lookup object
 async function fetchVms() {
   try {
-    const url = `${XO_HOST}/rest/v0/vms?fields=uuid,name_label,mainIpAddress,power_state`;
+    const url = `${XO_HOST}/rest/v0/vms?fields=$pool,name_label,mainIpAddress,power_state`;
     const vms = await fetchWithAuth(url);
-    const vmLookup = {};
-    for (const vm of vms) {
-      vmLookup[vm.uuid] = {
+    const vmLookup = vms.reduce((acc, vm) => {
+      if (!acc[vm.$pool]) {
+        acc[vm.$pool] = [];
+      }
+
+      acc[vm.$pool].push({
         name_label: vm.name_label,
         power_state: vm.power_state,
-        //mainIpAddress: vm.mainIpAddress
-      };
-    }
+        mainIpAddress: vm.mainIpAddress
+      });
+
+      return acc;
+    }, {});
+
     return vmLookup;
+
   } catch (err) {
     console.error('Error fetching VMs:', err);
     return {};
